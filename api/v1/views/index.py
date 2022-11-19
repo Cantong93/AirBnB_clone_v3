b4fc6253f372fce0643c0666178b8e2fbe327e65
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-19 11:43:04 +0000

    Add views routing

---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Routing for views"""
+from api.v1.views import app_views
+from flask import jsonify
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes-False)
+def show_status():
+    """Shows the status of the API
+         Returns:
+             A JSON string of the status
+    """
+    return jsonify({'status': 'OK'})
