32a2b3847ea8ca4f162140b5aff5d2a9a8bcaf41
Mason Mapfundematsva <masonmapfunde@gmail.com>
2022-11-19 12:09:48 +0000

    Task. 5: 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """Inintialize APIs"""
 from os import getenv
-from Flask import Flask, Blueprint
+from Flask import Flask, Blueprint, jsonify
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 
 
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 
 
@@ -16,6 +18,19 @@ app.register_blueprint(app_views)
 def tear_down(error):
     """Removes the SQLAlchemy session"""
     storage.close()
+
+
+@pp.errorhandler(404)
+def not_found(message):
+    """Handler for 404 errors.
+           Parameters:
+               message [str]: message to display.
+           Returns:
+               The HTTP response.
+    """
+    response = jsonify({"error": "Not found"})
+    response.satus_code = 404
+    return response
 
 
 if __name__ == "__main__":
